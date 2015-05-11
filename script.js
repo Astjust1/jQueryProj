89fd57016b293da5d293216b09614bc4e9cfdd24
Armond St.Juste <astjust1@binghamton.edu>
2015-05-11 00:11:27 -0400

    dsc

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /// <reference path="typings/jquery/jquery.d.ts"/>
 $(document).ready(function(){
 
-	for(var i = 0; i< 16;++i){
-				
+	for(var i = 0; i< 16;++i){	
 		var id = i+1;
-		
-		$("<div id='grid-"+id+"'>row1 "+id+"</div>").insertAfter( "#grid-"+i );
+		$("<div id='grid-"+id+"'>row "+id+"</div>").insertAfter( "#grid-"+i );
+	}
+	
+	for(var i = 0; i < 16;++i){
+		var id2 = i+1;
+		$()
 	}
 
 });
