e90c8def0b69e1d5b17c5eec0bc306fb4a0afbe9
Armond St.Juste <astjust1@binghamton.edu>
2015-05-10 23:23:31 -0400

    womp

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
 
 	for(var i = 0; i< 16;++i){
 		$(".container").append($("<div class='container2'>bar</div>"));
-		for(var j = 0; j < 16;++i){
-			$(".container2").append($("div class='container3'> foo</div>"));
-		}
 	}
 
 });
