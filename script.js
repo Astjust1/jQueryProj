25f857285ba661db591fbeca358bf49e625993dc
Armond St.Juste <astjust1@binghamton.edu>
2015-05-10 23:02:09 -0400

    first attempt at creating a grid

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	for(var i = 0; i< 16;++i){
+		$(".container").append($("<div class="container2"></div>"));
+		for(var j = 0; j < 16;++i){
+			$(".container2").append($(div class="container3"></div>));
+		}
+	}
+
+});
