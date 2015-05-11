5eb1f616cbf4fe196386a209ceea2d75e4975739
Armond St.Juste <astjust1@binghamton.edu>
2015-05-10 23:09:47 -0400

    blah

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+/// <reference path="typings/jquery/jquery.d.ts"/>
 $(document).ready(function(){
 
 	for(var i = 0; i< 16;++i){
-		$(".container").append($("<div class="container2"></div>"));
+		$(".container").append($("<div class='container2'></div>"));
 		for(var j = 0; j < 16;++i){
-			$(".container2").append($(div class="container3"></div>));
+			$(".container2").append($("div class='container3'></div>"));
 		}
 	}
 
