1249f1243b425df2812e9d24ab413f126df172d5
Armond St.Juste <astjust1@binghamton.edu>
2015-05-10 23:11:52 -0400

    YAAAAA

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 $(document).ready(function(){
 
 	for(var i = 0; i< 16;++i){
-		$(".container").append($("<div class='container2'></div>"));
+		$(".container").append($("<div class='container2'>bar</div>"));
 		for(var j = 0; j < 16;++i){
-			$(".container2").append($("div class='container3'></div>"));
+			$(".container2").append($("div class='container3'> foo</div>"));
 		}
 	}
 
