b30434d695859d778ba2c619820ef374141882db
Armond St.Juste <astjust1@binghamton.edu>
2015-05-12 20:05:20 -0400

    Cheese

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 
 	for(var i = 0; i< 16;++i){	
 		var id = i+1;
-		$("<div id='grid-"+id+"'>row "+id+"</div>").insertAfter( "#grid-"+i );
+		$("<div id='row-"+id+"'>row "+id+"</div>").insertAfter( "#row-"+i );
+		$("<div id=column-0>column0</div>").insertAfter("#row-"+i);
+		
 	}
 	
-	for(var i = 0; i < 16;++i){
-		var id2 = i+1;
-		$()
+	for(var j = 0; j < 16;++j){
+		var id2 = j+1;
+		$("<div id='column-"+id2+"'>column"+id2+"</div>").insertAfter("column-"+j);
 	}
 
 });
